b41be6237c1de353430d3ef7efb6839e7b0fc6b1
whaleism <230928187+whaleism@users.noreply.github.com>
2026-01-14 11:01:50 -0600

    Added footer and links

--- src/App.js
+++ src/App.js
@@ -7,6 +7,32 @@ export default function App() {
         <header className="App-header">
           <h1>Hello World!</h1>
         </header>
+        <footer className="text-center">
+          Coded by{" "}
+          <a
+            href="https://github.com/whaleism"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Jenny Lee
+          </a>
+          , on{" "}
+          <a
+            href="https://github.com/whaleism/jennys-react-dictionary-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
+          </a>
+          , hosted by{" "}
+          <a
+            href="https://thriving-sunflower-684c9e.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Netlify
+          </a>
+        </footer>
       </div>
     </div>
   );
