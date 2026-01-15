c3fdab9be9d6764e5b0739a921956d9ddf78ea90
whaleism <230928187+whaleism@users.noreply.github.com>
2026-01-15 11:53:37 -0600

    Added definition and sentence text to differentiate

--- src/Meaning.js
+++ src/Meaning.js
@@ -5,8 +5,8 @@ export default function Meaning(props) {
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
-      <p>{props.meaning.definition}</p>
-      <p>{props.meaning.example}</p>
+      <p>definition: {props.meaning.definition}</p>
+      <p>sentence example: {props.meaning.example}</p>
     </div>
   );
 }
