5b955b1c858183b0c18ce30f663cabf91d982dc9
whaleism <230928187+whaleism@users.noreply.github.com>
2026-01-16 09:28:31 -0600

    Created new Synonyms component to send meaning.synonyms to and will render one at a time.

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   console.log(props.meaning);
@@ -7,6 +8,19 @@ export default function Meaning(props) {
       <h3>{props.meaning.partOfSpeech}</h3>
       <p>definition: {props.meaning.definition}</p>
       <p>sentence example: {props.meaning.example}</p>
+      <br />
+      <Synonyms synonyms={props.meaning.synonyms} />
     </div>
   );
+  /* {props.results.definitions.map(function (definition, index) {
+          return (
+            <div key={index}>
+              <p>
+              {definition.definition}
+              <br />
+              <em>{definition.example}</em>
+            </p>
+            </div>
+          );
+        })} */
 }
